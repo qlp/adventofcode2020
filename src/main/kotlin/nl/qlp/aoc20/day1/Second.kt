cd7a38a067fd4eff60760b92ea0677f3ec596e2d
jurriaan <jurriaan@pruijs.nl>
2020-12-01 12:46:31 +0100

    Fix reuse self (now based on index instead of value)

--- src/main/kotlin/nl/qlp/aoc20/day1/Second.kt
+++ src/main/kotlin/nl/qlp/aoc20/day1/Second.kt
@@ -8,9 +8,9 @@ fun main(args: Array<String>) {
 
 class Second {
     fun run() = File("input.txt").readLines().map { it.toInt() }.let { numbers ->
-        numbers.flatMap { one ->
-            numbers.filter { it != one }.flatMap { two ->
-                numbers.filter { it != one && it != two }.map { three -> Triple(one, two, three)}
+        numbers.flatMapIndexed { i_one, one ->
+            numbers.filterIndexed { fi_one, _ -> fi_one != i_one }.flatMapIndexed { i_two, two ->
+                numbers.filterIndexed { fi_two, _ -> fi_two != i_two && fi_two != i_two }.map { three -> Triple(one, two, three)}
             }
         }.first { it.sum() == 2020 }
         .multiplication()
