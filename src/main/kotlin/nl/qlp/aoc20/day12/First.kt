b5a8d8bd5d6ef447d7d812ddf83908b5d5d82a8f
jurriaan <jurriaan@pruijs.nl>
2020-12-12 11:41:30 +0100

    Day 12 - cleanup

--- src/main/kotlin/nl/qlp/aoc20/day12/First.kt
+++ src/main/kotlin/nl/qlp/aoc20/day12/First.kt
@@ -5,13 +5,22 @@ fun main(args: Array<String>) {
 }
 
 class First {
-    fun run() = FirstShip(0, 0, Orientation.EAST).applyMoves().distance()
+    fun run() = FirstShip(orientation = Orientation.EAST).applyMoves().distance()
 }
 
-data class FirstShip(override val east: Int, override val north: Int, val orientation: Orientation): Ship {
+data class FirstShip(override val east: Int = 0, override val north: Int = 0, val orientation: Orientation): Ship {
     override fun apply(move: Move) = FirstShip(
             east + move.east + move.forward * orientation.east,
             north + move.north + move.forward * orientation.north,
             orientation.rotate(move.rotation)
     )
+}
+
+enum class Orientation(val east: Int, val north: Int) {
+    NORTH(0, 1),
+    EAST(1, 0),
+    SOUTH(0, -1),
+    WEST(-1, 0);
+
+    fun rotate(rotation: Int) = values()[(4 + ordinal + rotation / 90) % 4]
 }
--- src/main/kotlin/nl/qlp/aoc20/day12/Second.kt
+++ src/main/kotlin/nl/qlp/aoc20/day12/Second.kt
@@ -5,10 +5,10 @@ fun main(args: Array<String>) {
 }
 
 class Second {
-    fun run() = SecondShip(0, 0, Waypoint(10, 1)).applyMoves().distance()
+    fun run() = SecondShip(waypoint = Waypoint(10, 1)).applyMoves().distance()
 }
 
-data class SecondShip(override val east: Int, override val north: Int, val waypoint: Waypoint) : Ship {
+data class SecondShip(override val east: Int = 0, override val north: Int = 0, val waypoint: Waypoint) : Ship {
     override fun apply(move: Move) = SecondShip(
             east + waypoint.east * move.forward,
             north + waypoint.north * move.forward,
@@ -17,4 +17,14 @@ data class SecondShip(override val east: Int, override val north: Int, val waypo
                     waypoint.north + move.north
             ).rotate(move.rotation)
     )
+}
+
+data class Waypoint(val east: Int, val north: Int) {
+    fun rotate(rotation: Int) = when((rotation + 360) % 360) {
+        0 -> this
+        90 -> Waypoint(north, -east)
+        180 -> Waypoint(-east, -north)
+        270 -> Waypoint(-north, east)
+        else -> throw IllegalArgumentException("Unsupported rotation: $rotation")
+    }
 }
